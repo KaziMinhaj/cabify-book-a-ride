e648b614d5e2fc697e34aa7bdd7c4d05d9962313
Kazi Minhaj <kaziminhajulhaider@gmail.com>
2021-03-22 16:50:11 +0600

    matha nostho

--- src/components/EachTransport/EachTransport.js
+++ src/components/EachTransport/EachTransport.js
@@ -1,13 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router';
+import { allData } from '../FakeData/FakeData'
+import './EachTransport.css'
+import map from '../../images/Map.png'
+import SearchResult from './SearchResult';
 
 const EachTransport = () => {
     const { id } = useParams();
+    const [state, setState] = useState({
+        type: '',
+        img: ''
+
+    })
+    const [location, setLocation] = useState({
+        to: '',
+        from: '',
+        date: '',
+        isSubmitted: false
+    })
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        location.isSubmitted = !location.isSubmitted;
+    }
+    const handleBlur = (event) => {
+        location[event.target.name] = event.target.value
+        console.log(location);
+    }
     return (
-        <div>
-            <h1>Each transport</h1>
-            <h1>{id}</h1>
-        </div>
+        <div className="container">
+            {
+                allData.map(data => {
+                    if (data.id === parseFloat(id)) {
+                        state.type = data.type
+                        state.img = data.photo
+                    }
+                })
+            }
+            <div className="row">
+                <div className="col-sm search-box">
+                    <form action="" onSubmit={handleSubmit}>
+                        <label htmlFor="">Pick form</label>
+                        <br />
+                        <input onBlur={handleBlur} name="from" type="text" required />
+                        <br />
+                        <label htmlFor="">Pick form</label>
+                        <br />
+                        <input onBlur={handleBlur} name="to" type="text" required />
+                        <br />
+                        <label htmlFor="">Pick a date</label>
+                        <br />
+                        <input onBlur={handleBlur} name="date" type="date" required />
+                        <br />
+                        <input type="submit" className="btn btn-warning searchBtn" />
+                    </form>
+                </div>
+                <div className="col-sm map">
+                    <img src={map} alt="" />
+                </div>
+            </div>
+
+        </div >
     );
 };
 
--- src/components/Transports/Transport.js
+++ src/components/Transports/Transport.js
@@ -1,17 +1,20 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 import './Transport.css'
 
 const Transport = (props) => {
+    const { id, photo, type } = props.data
     return (
 
-        <Link to={`/home/${props.data.id}`}>
-            <div className="card eachCard" style={{ width: '18rem' }}>
-                <img src={props.data.photo} class="card-img-top" alt="..." />
-                <div className="card-body">
-                    <h5 className="card-text">{props.data.type}</h5>
+        <Link to={`/home/${id}`}>
+            <div className="row">
+                <div className="card eachCard col-sm" style={{ width: '18rem' }}>
+                    <img src={photo} class="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-text">{type}</h5>
+                    </div>
                 </div>
             </div>
+
         </Link>
 
     );
