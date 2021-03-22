7b140d769f9601bacdeafb4b3107f7e3d775af78
Kazi Minhaj <kaziminhajulhaider@gmail.com>
2021-03-22 23:53:44 +0600

    done

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -2,9 +2,13 @@ import React from 'react';
 import './Contact.css'
 
 const Contact = () => {
+    const style = {
+        textAlign: 'center',
+        padding: '50px'
+    }
     return (
-        <div className="contact">
-            <h3>Contact page</h3>
+        <div style={style} className="contact">
+            <h3> Contact: www.programminghero@gmail.com</h3>
         </div>
     );
 };
--- src/components/Destination/Destination.js
+++ src/components/Destination/Destination.js
@@ -3,8 +3,12 @@ import { UserContext } from '../../App';
 import './Destination.css'
 const Destination = () => {
     const [loggedInUser, setloggedInUser] = useContext(UserContext)
+    const style = {
+        textAlign: 'center',
+        padding: '50px'
+    }
     return (
-        <div className="destinaiton">
+        <div style={style} className="destinaiton">
             <h3>Welcome to Destinaiton page {loggedInUser.name}</h3>
             <h5>This is a priavte place thats why without login people cant access here.</h5>
 
